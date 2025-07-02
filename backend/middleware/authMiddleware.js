fa2e7e567e492501d39b1c4c27b2a86b347446a9
nirajandhungel <subashdhungel555@gmail.com>
2025-07-02 06:54:25 +0545

    Created  2 middlewares protect and protectAdmin

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,3 +1,31 @@
-export const  protect = (req, re, next)=>{
+import User from "../model/User.js";
+import jwt from "jsonwebtoken"
 
+export const protect = async (req, res, next) => {
+  try {
+    const token = req.cookies?.token; // get token from cookie
+    if (!token) {
+      return res.status(401).json({ error: "Not authorized, no token" });
+    }
+
+    // verify the token using JWT_SECRET
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); // returns payload (e.g., {id,iat,exp})
+
+    // get the user from DB using id in the token payload
+    const user = await User.findById(decoded.id).select("-password");
+    if (!user) {
+      return res.status(400).json({ messagee: "User not found" });
+    }
+    req.user = user; // Attaching user to request
+    next();
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+};
+
+export const protectAdmin = async (req, res, next)=>{
+    if(!res.user.role=='admin'){
+        return res.status(400).json({message:'Only admin'})
+    }
+    next();
 }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { loginController, signupController, logoutController } from '../controllers/authControllers.js';
 import { adminDashboardController, clientDashboardController } from '../controllers/dashboardControllers.js';
+import { protect, protectAdmin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/login',loginController);
 router.post('/signup',signupController);
 router.post('/logout',logoutController);
-router.post('/admin/dashboard',adminDashboardController);
-router.post('/home',clientDashboardController);
+router.post('/admin/dashboard',protect,protectAdmin, adminDashboardController);
+router.post('/home',protect,clientDashboardController);
 export default router;
